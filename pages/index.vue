a12e5f38eb2ddfb7edaf5ca69e3c338405fe3838
金田尚大 <1111510078r21@gmail.com>
2019-11-19 23:17:37 +0900

    rest apiで店舗情報を取得して表示

--- pages/index.vue
+++ pages/index.vue
@@ -1,53 +1,44 @@
 <template>
   <section class="section">
     <div class="columns is-mobile">
-      <card
-        title="Free"
-        icon="github-circle"
-      >
-        Open source on <a href="https://github.com/buefy/buefy">
-          GitHub
-        </a>
-      </card>
-
-      <card
-        title="Responsive"
-        icon="cellphone-link"
-      >
-        <b class="has-text-grey">
-          Every
-        </b> component is responsive
-      </card>
-
-      <card
-        title="Modern"
-        icon="alert-decagram"
-      >
-        Built with <a href="https://vuejs.org/">
-          Vue.js
-        </a> and <a href="http://bulma.io/">
-          Bulma
-        </a>
-      </card>
-
-      <card
-        title="Lightweight"
-        icon="arrange-bring-to-front"
-      >
-        No other internal dependency
-      </card>
+      <div class="card" v-for="shop in shops" :key="shop.id">
+        <div class="card-image">
+          <figure class="image is-4by3">
+            <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image" />
+          </figure>
+        </div>
+        <div class="card-content">
+          <div class="media">
+            <div class="media-content">
+              <p class="title is-4">{{ shop.name }}</p>
+            </div>
+          </div>
+          <div class="content">
+            <p>{{ shop.address }}</p>
+            <a href="tel">{{ shop.tel }}</a>
+          </div>
+          <div class="buttons">
+            <b-button type="is-primary" expanded>detail</b-button>
+          </div>
+        </div>
+      </div>
     </div>
   </section>
 </template>
 
 <script>
-import Card from '~/components/Card'
-
 export default {
-  name: 'HomePage',
-
-  components: {
-    Card
+  data() {
+    return {
+      shops: []
+    };
+  },
+  async asyncData({ app }) {
+    const apiUrl = "http://127.0.0.1:8088/api/v1/shops";
+    const res = await app.$axios.$get(apiUrl);
+    return {
+      shops: res
+    };
   }
-}
+};
 </script>
